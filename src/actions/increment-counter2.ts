99ab6ea474a034187bffbbf8dcb8f10bfd213c7f
Alex Patterson <alex@codingcat.dev>
2025-12-18 17:03:05 -0500

    understand counter better

--- src/actions/increment-counter2.ts
+++ src/actions/increment-counter2.ts
@@ -3,12 +3,12 @@ import { action, type KeyDownEvent, SingletonAction, WillAppearEvent, streamDeck
 /**
  * An example action class that displays a count that increments by one each time the button is pressed.
  */
-@action({ UUID: "dev.codingcat.streamdeck.increment" })
-export class IncrementCounter extends SingletonAction<CounterSettings> {
+@action({ UUID: "dev.codingcat.streamdeck.increment2" })
+export class IncrementCounter2 extends SingletonAction<CounterSettings> {
 	constructor() {
 		super();
 		streamDeck.settings.onDidReceiveGlobalSettings<CounterSettings>((ev) => {
-			streamDeck.logger.debug('onDidReceiveGlobalSettings-IncrementCounter', ev);
+			streamDeck.logger.debug('onDidReceiveGlobalSettings-IncrementCounter2', ev);
 			this.actions.forEach(action => {
 				action.setTitle(`${ev.settings.count ?? 0}`);
 			});
@@ -17,7 +17,7 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 
 	override async onWillAppear(ev: WillAppearEvent<CounterSettings>): Promise<void> {
 		let { count } = await streamDeck.settings.getGlobalSettings<CounterSettings>();
-		streamDeck.logger.debug('onWillAppear-IncrementCounter', { count });
+		streamDeck.logger.debug('onWillAppear-IncrementCounter2', { count });
 		await ev.action.setTitle(`${count ?? 0}`);
 	}
 
@@ -27,7 +27,7 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 		const incrementBy = currentIncrementBy ?? 1;
 		const count = (currentCount ?? 0) + incrementBy;
 
-		streamDeck.logger.debug('onKeyDown-IncrementCounter', { count, incrementBy });
+		streamDeck.logger.debug('onKeyDown-IncrementCounter2', { count, incrementBy });
 		await streamDeck.settings.setGlobalSettings({ count, incrementBy });
 
 		//TODO: This is a workaround to ensure the settings updating calls onDidReceiveGlobalSettings.
@@ -36,7 +36,7 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 }
 
 /**
- * Settings for {@link IncrementCounter}.
+ * Settings for {@link IncrementCounter2}.
  */
 type CounterSettings = {
 	count?: number;
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,14 @@
 import streamDeck from "@elgato/streamdeck";
 
 import { IncrementCounter } from "./actions/increment-counter";
-
+import { IncrementCounter2 } from "./actions/increment-counter2";
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel("trace");
+// streamDeck.settings.useExperimentalMessageIdentifiers = true;
 
 // Register the increment action.
 streamDeck.actions.registerAction(new IncrementCounter());
+streamDeck.actions.registerAction(new IncrementCounter2());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
